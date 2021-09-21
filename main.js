9498bc4a2d37880dc9d62dc20b8ef77974f516b8
Nathan Little <nathanlittle@Nathans-MacBook-Air.local>
2021-09-21 16:43:24 -0400

    questions 2 and 3 completed

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ let average = (items.reduce((accum, element) => {
 },0) / items.length).toFixed(2);
 
 console.log(`$${average}`);
+
+
+
+let specificItems = items.filter(element => {
+    return element.price >= 14 && element.price <= 18;
+});
+
+console.log(`Items between $14.00 and $18.00:\n`, specificItems);
+
+
+
+let itemsGBP = items.filter(element => {
+    return element.currency_code === "GBP"
+});
+
+console.log(`${itemsGBP[0].title} costs ${itemsGBP[0].price}`)
