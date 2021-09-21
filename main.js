7e20e9c24585a690529b60010e7d8c55b6d6ca6b
Nathan Little <nathanlittle@Nathans-MacBook-Air.local>
2021-09-21 16:48:39 -0400

    final commit

--- main.js
+++ main.js
@@ -19,3 +19,34 @@ let itemsGBP = items.filter(element => {
 });
 
 console.log(`${itemsGBP[0].title} costs ${itemsGBP[0].price}`)
+
+
+
+let woodenItems = items.filter(element => {
+    return element.materials.includes(`wood`);
+}).forEach(element => {
+    console.log(`${element.title} is made of wood`)
+}); 
+
+
+
+let eightOrMore = items.filter(element => {
+    console.log
+    return element.materials.length >= 8;
+}).forEach(element => {
+    console.log(`${element.title} has ${element.materials.length} materials`);
+    element.materials.forEach(element => {
+        console.log(`${element}`)
+    });
+});
+
+
+
+let madeBySellers = items.reduce((accum, element) => {
+    if (element['who_made'] === 'i_did') {
+        return accum + 1;
+    }
+    return accum;
+}, 0);
+
+console.log(`${madeBySellers} were made by their sellers`);
